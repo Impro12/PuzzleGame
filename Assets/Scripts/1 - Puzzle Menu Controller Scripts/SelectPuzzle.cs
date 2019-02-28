5b0cabdf971558bb48cff04fd697be5bb76e5cc2
Vitalii Holko <impro9712@gmail.com>
2019-02-28 20:20:18 +0200

    UI(need to make Puzzle Level Buttons

---
+++ Assets/Scripts/1 - Puzzle Menu Controller Scripts/SelectPuzzle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectPuzzle : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject selectPuzzleMenuPanel, puzzleLevelSelectPanel;
+
+    [SerializeField]
+    private Animator selectPuzzleMenuAnim, puzzleLevelSelectAnim;
+
+    private string selectedPuzzle;
+
+    public void SelectedPuzzle()
+    {
+        selectedPuzzle = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
+
+        StartCoroutine(ShowPuzzleMenuSelectMenu ());
+    }
+
+    
+    
+
+    IEnumerator ShowPuzzleMenuSelectMenu()
+    {
+        puzzleLevelSelectPanel.SetActive(true);
+        selectPuzzleMenuAnim.Play("SlideOut");
+        puzzleLevelSelectAnim.Play("SlideIn");
+        yield return new WaitForSeconds(1f);
+        selectPuzzleMenuPanel.SetActive(false);
+    }
+}
